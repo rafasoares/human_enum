46a445696d76e5b20bb5aaa0c2ee6258a49b870d
Rafael Soares <893971+rafasoares@users.noreply.github.com>
2025-04-17 08:30:29 +0200

    test: refactor multiple model spec and add test for instance methods

--- spec/human_enum_spec.rb
+++ spec/human_enum_spec.rb
@@ -19,10 +19,10 @@ RSpec.describe HumanEnum do
                          red: 'Red', blue: 'Violets are blue'
       end
 
-      it_behaves_like 'multiple models using the same enum name',
-                      :scope,
-                      first: { user: 'User scope', account: 'Account scope', project: 'Project scope' },
-                      second: { city: 'City scope', state: 'State scope', country: 'Country scope' }
+      include_examples 'multiple models',
+                       :scope,
+                       first: { user: 'User scope', account: 'Account scope', project: 'Project scope' },
+                       second: { city: 'City scope', state: 'State scope', country: 'Country scope' }
     end
   end
 
@@ -40,10 +40,10 @@ RSpec.describe HumanEnum do
                          north: 'Up?', south: 'South', east: 'East', west: 'West'
       end
 
-      it_behaves_like 'multiple models using the same enum name',
-                      :status,
-                      first: { active: 'First Active', inactive: 'First Inactive' },
-                      second: { pending: 'Second Pending', completed: 'Second Completed' }
+      include_examples 'multiple models',
+                       :status,
+                       first: { active: 'First Active', inactive: 'First Inactive' },
+                       second: { pending: 'Second Pending', completed: 'Second Completed' }
     end
   end
 end
